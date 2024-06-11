7cb8996e21d8a3faae8765fd9d155ef8d5ba771d
Patrick <mitchellpatrick667@gmail.com>
2024-06-11 16:33:59 -0400

    Create Godot Project, initial exploration/setup
    The most notable addition is the "Configuration" node/scene, which is set to be auto loaded, and thus be a hub for things like what colors the numbers can be. Since I plan to expand on this project after the jam, (or rather make another project with the same core mechanics), this kind of set up will allow an options menu where colors can be configured by the user rather than just me, improving accessibility.

---
+++ Scripts/configuration.gd
@@ -0,0 +1,14 @@
+extends Node
+
+@export var numberColors : Array[Color] = [
+	Color.WHITE,
+	Color.SADDLE_BROWN,
+	Color.RED,
+	Color.DARK_ORANGE,
+	Color.YELLOW,
+	Color.GREEN,
+	Color.CYAN,
+	Color.BLUE,
+	Color.PURPLE,
+	Color.HOT_PINK,
+]
